deb0285624b0021adc34d7cb088ae95e566ea71f
Debdeep Kundu <debdeepkundu643@gmail.com>
2026-01-26 01:51:31 +0530

    nginx changes

--- Frontend/Food Website/Food Website/user/vite.config.js
+++ Frontend/Food Website/Food Website/user/vite.config.js
@@ -1,24 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   alias: {
-//     '@components': '/src/components',
-//   },
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',          // IMPORTANT: root path
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-    },
+  alias: {
+    '@components': '/src/components',
   },
 })
+
--- Frontend/FoodOwner/FoodOwner/foodowner/vite.config.js
+++ Frontend/FoodOwner/FoodOwner/foodowner/vite.config.js
@@ -1,23 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   alias: {
-//     '@components': '/src/components',
-//   },
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/foodowner/',  // IMPORTANT: matches Nginx location
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-    },
+  alias: {
+    '@components': '/src/components',
   },
 })
